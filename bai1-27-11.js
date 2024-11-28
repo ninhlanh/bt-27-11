f601224e14887b6abfbe2123c4cc833d319a956b
ninhlanh <nt.lanhh18@gmail.com>
2024-11-28 11:58:39 +0700

    27-11

---
+++ bai1-27-11.js
@@ -0,0 +1,11 @@
+function checkAge(age){  //  checkage :tên hàm : chữ đầu viết thường chữ sau viết hoa ,age: biến 
+    if(age>18){
+         console.log("nguoi lớn");
+        // return "người lớn"
+    }else{
+         console.log("vị thành niên");
+        // return "vị thành niên"
+    }
+}
+// console.log();
+checkAge(age=18);
